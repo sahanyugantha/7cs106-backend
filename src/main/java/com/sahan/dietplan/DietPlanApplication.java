6d22bc42ac9eed57ea484b3375b9a11bf428f5ca
Sahan Perera <sahanyuganthamail@gmail.com>
2024-07-22 21:32:19 +0100

    init

---
+++ src/main/java/com/sahan/dietplan/DietPlanApplication.java
@@ -0,0 +1,13 @@
+package com.sahan.dietplan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DietPlanApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DietPlanApplication.class, args);
+    }
+
+}
